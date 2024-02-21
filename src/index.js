752c9c7d1e3001d3d6d27b06c5cb76e6cbd7da41
Oleksander Novak <sandrix28101981@gmail.com>
2024-02-21 15:36:06 +0200

    fixed webpack config

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import Phaser from 'phaser';
 const scene = new Phaser.Scene('Game');
 
 scene.preload = function () {
-	this.load.image('bg', 'assets/image/bg-stars.jpg');
+	this.load.image('bg', 'src/assets/image/bg-stars.jpg');
 };
 
 scene.create = function () {
--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,74 @@
 const path = require('path');
-const webpackMerge = require('webpack-merge');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('css-minimizer-webpack-plugin');
 const FriendlyErrorsWebpackPlugin = require('@soda/friendly-errors-webpack-plugin');
 const WebpackBar = require('webpackbar');
 
-const loadModeConfig = env => require(`webpack/${env.mode}.config`)(env);
+module.exports = (env, argv) => {
+	const isProduction = argv.mode === 'production';
 
-module.exports = env =>
-	webpackMerge(
-		{
-			mode: env.mode,
-			context: path.resolve(__dirname, 'src'),
-			entry: {
-				index: './index.js',
-			},
-			output: {
-				path: path.resolve(__dirname, 'dist'),
-				filename: '[name].bundle.js',
-			},
-			module: {
-				rules: [
-					{
-						test: /\.js$/,
-						exclude: /node_modules/,
-						use: ['babel-loader'],
-					},
-					{
-						test: /\.(gif|png|jpe?g|svg)$/i,
-						use: [
-							{
-								loader: 'url-loader',
-								options: {
-									name: '[path]/[name].[ext]',
-									limit: 5000,
-								},
-							},
-						],
-					},
-					{
-						test: /\.html$/,
-						use: 'html-loader',
-					},
-					{
-						test: /\.hbs$/,
-						use: 'handlebars-loader',
-					},
-				],
-			},
-			plugins: [
-				new CleanWebpackPlugin(),
-				new FriendlyErrorsWebpackPlugin(),
-				new WebpackBar(),
+	return {
+		mode: argv.mode,
+		context: path.resolve(__dirname, 'src'),
+		entry: {
+			index: ['./index.js', './sass/main.scss'],
+		},
+		output: {
+			path: path.resolve(__dirname, 'dist'),
+			filename: '[name].bundle.js',
+		},
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					use: ['babel-loader'],
+				},
+				{
+					test: /\.(gif|png|jpe?g|svg)$/i,
+					type: 'asset/resource',
+				},
+				{
+					test: /\.html$/,
+					use: 'html-loader',
+				},
+				{
+					test: /\.scss$/i,
+					use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+				},
+				{
+					test: /\.hbs$/,
+					use: 'handlebars-loader',
+				},
 			],
 		},
-		loadModeConfig(env)
-	);
+		plugins: [
+			new CleanWebpackPlugin(),
+			new HtmlWebpackPlugin({
+				template: './index.html',
+				minify: isProduction,
+			}),
+			new MiniCssExtractPlugin({
+				filename: 'main.min.css',
+			}),
+			new OptimizeCssAssetsPlugin(),
+			new FriendlyErrorsWebpackPlugin(),
+			new WebpackBar(),
+		].filter(Boolean),
+		devtool: isProduction ? 'source-map' : 'cheap-module-source-map',
+		devServer: {
+			static: {
+				directory: path.join(__dirname, 'src'),
+			},
+			historyApiFallback: true,
+			compress: true,
+			port: 4040,
+			client: {
+				logging: 'error',
+			},
+			open: true,
+		},
+	};
+};
